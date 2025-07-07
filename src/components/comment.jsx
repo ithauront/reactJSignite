42714195db69079973b70cc5e3d909c226c9019e
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-07-07 11:01:18 +0200

    config[root]: prepare for deploy

--- src/components/comment.jsx
+++ src/components/comment.jsx
@@ -2,24 +2,24 @@ import styles from './comment.module.css'
 import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './avatar'
 
-export function Comment ({content}) {
+export function Comment({ content }) {
     return (
         <div className={styles.comment}>
-            <Avatar hasBorder={false} src="https://source.unsplash.com/random"/>
+            <Avatar hasBorder={false} src="https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=417&q=80" />
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
                     <header>
-                       <div className={styles.authorAndTime}>
+                        <div className={styles.authorAndTime}>
                             <strong>Iuri Reis</strong>
                             <time title='11 de março às 11:26h' dateTime='2023-03-11 11:26:32'>Cerca de 2h atras </time>
- 
-                       </div>
-                       <button title='Delete comment'>
-                                <Trash size={24}/>
-                            </button>
+
+                        </div>
+                        <button title='Delete comment'>
+                            <Trash size={24} />
+                        </button>
                     </header>
                     <p>
-                       {content}
+                        {content}
                     </p>
                 </div>
                 <footer>
@@ -28,11 +28,11 @@ export function Comment ({content}) {
                         Aplaudir <span>20</span>
                     </button>
                 </footer>
-           </div>
+            </div>
         </div>
 
-    
-        
+
+
     )
 
 }
